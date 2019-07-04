d1ee20731a939ad959dc8237982a85f8d9bcc0f6
Georgiy-Vwfe <31819579+Georgiy-Vwfe@users.noreply.github.com>
2019-07-04 19:32:10 +0300

    delete redurant files

--- complete/src/main/java/hello/FileUploadController.java
+++ complete/src/main/java/hello/FileUploadController.java
@@ -1,8 +1,7 @@
 package hello;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
-
+import hello.storage.StorageFileNotFoundException;
+import hello.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -10,17 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import hello.storage.StorageFileNotFoundException;
-import hello.storage.StorageService;
+import java.io.File;
+import java.io.IOException;
+import java.util.stream.Collectors;
 
 @Controller
 public class FileUploadController {
@@ -54,18 +50,35 @@ public class FileUploadController {
 
     @PostMapping("/")
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
-            RedirectAttributes redirectAttributes) {
+                                   RedirectAttributes redirectAttributes) {
+        if (getFileExtension(file).equalsIgnoreCase("xlsx")){
+            storageService.store(file);
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded " + file.getOriginalFilename() + "!");
 
-        storageService.store(file);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
+            return "redirect:/";
+        } else {
+            redirectAttributes.addFlashAttribute("message",
+                    "You can't upload " + file.getOriginalFilename() + "!");
+            return "redirect:/";
+        }
 
-        return "redirect:/";
     }
 
     @ExceptionHandler(StorageFileNotFoundException.class)
     public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
         return ResponseEntity.notFound().build();
+    }
+
+    private static String getFileExtension(MultipartFile f) {
+        File file = (File) f;
+        String fileName = f.getName();
+
+        if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0) {
+            return fileName.substring(fileName.lastIndexOf(".") + 1);
+        } else {
+            return "";
+        }
     }
 
 }
